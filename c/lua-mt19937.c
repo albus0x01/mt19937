446d3766da54b1ec6c84bab3a11a3e44efca0f4b
ldy105cn <2050l@163.com>
2016-11-18 11:00:27 +0800

    lua version

---
+++ c/lua-mt19937.c
@@ -0,0 +1,61 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include<mt19937-64.h>
+
+static int lrandomseed(lua_State *L){
+    mt19937 *mt = lua_touserdata(L, lua_upvalueindex(1));
+    uint32_t seed = (uint32_t)luaL_checknumber(L, 1);
+    mt19937_seed(mt, seed);
+    return 0;
+}
+
+static int lrandom(lua_State *L){
+    mt19937 *mt = lua_touserdata(L, lua_upvalueindex(1));
+    uint64_t r = mt19937_uint64(mt);
+    lua_Integer low, up;
+    switch(lua_gettop(L)){
+        case 0:{
+            lua_pushnumber(L, (lua_Number)r);
+            return 1;
+        }
+        case 1:{
+            low = 1;
+            up = luaL_checkinteger(L, 1);
+            break;
+        }
+        case 2:{
+            low = luaL_checkinteger(L, 1);
+            up = luaL_checkinteger(L, 2);
+            break;
+        }
+        default:{
+            return luaL_error(L, "wrong number of arguments");
+        }
+    }
+    r = r % (up - low) + low;
+    lua_pushinteger(L, r);
+    return 1;
+}
+
+static int lgc(lua_State *L){
+    printf("mt19937 gc");
+    mt19937 *mt = lua_touserdata(L, lua_upvalueindex(1));
+    mt19937_free(mt);
+    return 0;
+}
+
+int
+luaopen_mt19937(lua_State *L){
+    luaL_checkversion(L);
+    luaL_Reg l[] = {
+        {"randomseed", lrandomseed},
+        {"random", lrandom},
+        {"__gc", lgc},
+        {NULL, NULL},
+    };
+    luaL_newlibtable(L, l);
+    mt19937 *mt = mt19937_new();
+    lua_pushlightuserdata(L, mt);
+    luaL_setfuncs(L, l, 1);
+    return 1;
+}
