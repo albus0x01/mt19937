f7abaf1eda02efd89d0e12cae374fd443bcd9526
ldy105cn <2050l@163.com>
2016-11-28 15:32:44 +0800

    multi use

--- lua/test.lua
+++ lua/test.lua
@@ -1,9 +1,28 @@
 -- package.cpath = "../../../luaclib/?.so"
 local mt19937 = require("mt19937")
 
-function main()
+function normal()
     mt19937.randomseed(100)
     print(mt19937.random(100, 100000))
+end
+
+function multi()
+    player1.mt19937 = require("mt19937")
+    player1.mt19937.randomseed(100)
+    local player2 = {}
+    package.loaded["mt19937"] = nil
+    player2.mt19937 = require("mt19937")
+    --  print(player1.mt19937, player2.mt19937)
+    player2.mt19937.randomseed(500)
+    print(player1.mt19937.random(1, 100))
+    print(player1.mt19937.random(1, 100))
+    print(player2.mt19937.random(1, 100))
+    print(player2.mt19937.random(1, 100))
+    print(player2.mt19937.random(1, 100))
+end
+
+function main()
+    normal()
 end
 
 main()
